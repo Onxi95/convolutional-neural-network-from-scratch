a7bcb63fa27797b60d1205ae4ee41cbe2de08420
Paweł Sowa <pawel@the-sowa.com>
2024-03-30 10:42:10 +0100

    feat: add stdout to logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+import sys
 import os
 
 now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -8,5 +9,8 @@ if not os.path.exists('logs'):
     os.makedirs('logs')
 
 logger = logging.getLogger(__name__)
+logger.addHandler(
+    logging.StreamHandler(sys.stdout))
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s\n%(message)s\n', filename=f'logs/{now}.log')
