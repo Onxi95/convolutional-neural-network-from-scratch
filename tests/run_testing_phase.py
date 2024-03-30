b26ec94469734f5f53c600f09afe0b4604648692
Paweł Sowa <pawel@the-sowa.com>
2024-03-30 09:32:02 +0100

    feat: add confusion matrix

--- tests/run_testing_phase.py
+++ tests/run_testing_phase.py
@@ -27,21 +27,28 @@ def run_testing_phase(
         tuple: A tuple containing the test loss and test accuracy.
 
     """
+    confusion_matrix = np.zeros((10, 10), dtype=int)
+
     print("Testing phase...")
     total_loss = 0
     total_correct = 0
 
     for image, label in zip(test_images, test_labels):
         image_array = image.reshape(img_size, img_size)
-        _, loss, correct = perform_forward_pass(
+        softmax_probs, loss, correct = perform_forward_pass(
             image_array, label, convolution_layer, max_pooling_layer, softmax_output_layer)
         total_loss += loss
         total_correct += correct
+        predicted_label = np.argmax(softmax_probs)
+        confusion_matrix[label, predicted_label] += 1
 
     num_tests = len(test_images)
     test_loss = total_loss / num_tests
     test_accuracy = total_correct / num_tests
     print(f'Test Loss: {test_loss}')
     print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
+
+    print("Confusion Matrix:")
+    print(confusion_matrix)
 
     return test_loss, test_accuracy
